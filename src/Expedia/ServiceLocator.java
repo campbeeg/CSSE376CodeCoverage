3fc47a329b58062fa0dfef90384300963cf9e5b9
Ethan Campbell <campbeeg@rose-hulman.edu>
2015-04-17 01:58:10 -0400

    User has 100% Term Coverage

--- src/Expedia/ServiceLocator.java
+++ src/Expedia/ServiceLocator.java
@@ -44,6 +44,11 @@ public class ServiceLocator {
 		discounts.add(aDiscount);	
 	}
 	
+	public void ClearDiscount() 
+	{
+		discounts.clear();
+	}
+	
 	public void AddFlight(Flight aFlight)
 	{
 		flights.add(aFlight);	
--- src/ExpediaTest/UserTest.java
+++ src/ExpediaTest/UserTest.java
@@ -106,6 +106,27 @@ public class UserTest {
 		this.target.book(new Booking[] { new Flight(StartDate, EndDate, 100),
 				new Hotel(5), new Car(3) });
 		assertEquals(1024.65, this.target.Price(), 0.01);
+		ServiceLocator.Instance().ClearDiscount();
+	}
+	
+	@Test
+	public void TestThatDiscountFirstFalse() {
+		Discount target = new Discount(0.00, 1);
+		ServiceLocator.Instance().AddDiscount(target);
+		this.target.book(new Booking[] { new Flight(StartDate, EndDate, 100),
+				new Hotel(5), new Car(3) });
+		assertEquals(1035.0, this.target.Price(), 0.01);
+		ServiceLocator.Instance().ClearDiscount();
+	}
+	
+	@Test
+	public void TestThatDiscountSecondFalse() {
+		Discount target = new Discount(0.01, 101);
+		ServiceLocator.Instance().AddDiscount(target);
+		this.target.book(new Booking[] { new Flight(StartDate, EndDate, 100),
+				new Hotel(5), new Car(3) });
+		assertEquals(1035.0, this.target.Price(), 0.01);
+		ServiceLocator.Instance().ClearDiscount();
 	}
 
 	@After
